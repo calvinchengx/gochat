8b9ec03f4f6bca9d5ab410fccb507cbe66b1a790
Calvin Cheng <linchuan.cheng@gmail.com>
2018-10-21 18:57:48 +0800

    Let's see if our javascript web test works

---
+++ client/web/pb/ChatService_grpc_web_pb.js
@@ -0,0 +1,78 @@
+/**
+ * @fileoverview gRPC-Web generated client stub for proto
+ * @enhanceable
+ * @public
+ */
+
+// GENERATED CODE -- DO NOT EDIT!
+
+
+
+const grpc = {};
+grpc.web = require('grpc-web');
+
+
+var ChatMessage_pb = require('./ChatMessage_pb.js')
+const proto = {};
+proto.proto = require('./ChatService_pb.js');
+
+/**
+ * @param {string} hostname
+ * @param {?Object} credentials
+ * @param {?Object} options
+ * @constructor
+ * @struct
+ * @final
+ */
+proto.proto.ChatClient =
+    function(hostname, credentials, options) {
+  if (!options) options = {};
+  options['format'] = 'text';
+
+  /**
+   * @private @const {!grpc.web.GrpcWebClientBase} The client
+   */
+  this.client_ = new grpc.web.GrpcWebClientBase(options);
+
+  /**
+   * @private @const {string} The hostname
+   */
+  this.hostname_ = hostname;
+
+  /**
+   * @private @const {?Object} The credentials to be used to connect
+   *    to the server
+   */
+  this.credentials_ = credentials;
+
+  /**
+   * @private @const {?Object} Options for the client
+   */
+  this.options_ = options;
+};
+
+
+/**
+ * @param {string} hostname
+ * @param {?Object} credentials
+ * @param {?Object} options
+ * @constructor
+ * @struct
+ * @final
+ */
+proto.proto.ChatPromiseClient =
+    function(hostname, credentials, options) {
+  if (!options) options = {};
+  options['format'] = 'text';
+
+  /**
+   * @private @const {!proto.proto.ChatClient} The delegate callback based client
+   */
+  this.delegateClient_ = new proto.proto.ChatClient(
+      hostname, credentials, options);
+
+};
+
+
+module.exports = proto.proto;
+
--- generate.sh
+++ generate.sh
@@ -15,10 +15,17 @@ go build -v github.com/golang/protobuf/protoc-gen-go
 # set path so that we can invoke our installed tools
 export PATH=$PATH:$PWD
 
-# prepare the protobuf directory
+# prepare the protobuf directory - golang
 mkdir -p pb
 protoc -I=proto \
     --go_out=plugins=grpc,import_path=pb:pb/ \
+    proto/*.proto
+
+# prepare the protobuf directory - web/javascript
+mkdir -p client/web/pb
+protoc -I=proto \
+    --js_out=import_style=commonjs:client/web/pb \
+    --grpc-web_out=import_style=commonjs,mode=grpcwebtext:client/web/pb \
     proto/*.proto
 
 # remove our protoc-gen-go tool
